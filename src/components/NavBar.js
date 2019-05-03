1d47722689e9eecc39de2b0ba9c1d7565ec99e85
github username <github email address>
2019-05-03 11:55:33 +0100

    Styling on navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,11 +3,21 @@ import React, {Component} from 'react';
 class NavBar extends Component {
   render() {
     const {returnToGalaxy, goToSolarSystem, goToPlanetView} = this.props
-    return (<div>
-      <button className="ui button green" onClick={returnToGalaxy}>Galaxy View</button>
-      <button className="ui button green" onClick={goToSolarSystem}>Solar System View</button>
-      <button className="ui button green" onClick={goToPlanetView}>Planet View</button>
-    </div>);
+    return (
+      <div id="navbar" className="ui grid middle aligned">
+        <div className="four wide column" >
+          <img id="logo" src={require("../images/logo.png")} />
+        </div>
+        <div className="eight wide column" id="buttoon">
+          <button className="ui button white" onClick={returnToGalaxy}>Galaxy View</button>
+          <button className="ui button white" onClick={goToSolarSystem}>Solar System View</button>
+          <button className="ui button white" onClick={goToPlanetView}>Planet View</button>
+        </div>
+        <div className="four wide column">
+
+        </div>
+      </div>
+    )
   }
 }
 
