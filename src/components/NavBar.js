8de48c762b9c2e3ec597649a41080d53d91114a8
github username <github email address>
2019-05-03 09:59:45 +0100

    Beautify

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class NavBar extends Component {
-    render() {
-        const { returnToGalaxy, goToSolarSystem, goToPlanetView} = this.props
-        return (
-            <div>
-               <button onClick={returnToGalaxy}>Galaxy View</button> 
-               <button onClick={goToSolarSystem}>Solar System View</button> 
-               <button onClick={goToPlanetView}>Planet View</button> 
-            </div>
-        );
-    }
+  render() {
+    const {returnToGalaxy, goToSolarSystem, goToPlanetView} = this.props
+    return (<div>
+      <button onClick={returnToGalaxy}>Galaxy View</button>
+      <button onClick={goToSolarSystem}>Solar System View</button>
+      <button onClick={goToPlanetView}>Planet View</button>
+    </div>);
+  }
 }
 
 export default NavBar;
--- src/components/Planet.js
+++ src/components/Planet.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Planet extends Component {
 
-    
-    render() {
-        const {returnToSolarSystem, selectedPlanet} = this.props
+  render() {
+    const {returnToSolarSystem, selectedPlanet} = this.props
 
-        return (
-            <div>
-                <h1>{selectedPlanet.charAt(0).toUpperCase() + selectedPlanet.slice(1)}</h1>
-                <img className={selectedPlanet} src={require(`../images/${this.props.selectedPlanet}.png`)} /> <br/>
-                <button onClick={this.props.returnToSolarSystem}>Back to Solar System view</button>
-            </div>
-        );
-    }
+    return (<div>
+      <h1>{selectedPlanet.charAt(0).toUpperCase() + selectedPlanet.slice(1)}</h1>
+      <img alt="Selected Planet" className={selectedPlanet} src={require(`../images/${this.props.selectedPlanet}.png`)}/>
+      <br/>
+      <button onClick={this.props.returnToSolarSystem}>Back to Solar System view</button>
+    </div>);
+  }
 }
 
 export default Planet;
