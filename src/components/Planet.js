8a477f6a29705388128e393dc77f46ef9c57678d
github username <github email address>
2019-05-08 14:17:14 +0100

    Minor changes

--- src/components/Planet.js
+++ src/components/Planet.js
@@ -14,6 +14,12 @@ class Planet extends Component {
       flexDirection: 'row',
       justifyContent: 'center'
     }
+    const paddingLeft = {
+      paddingLeft: '20px'
+    }
+    const paddingFifty = {
+      padding: '50px'
+    }
 
 
     const {returnToSolarSystem, selectedPlanet} = this.props
@@ -22,15 +28,19 @@ class Planet extends Component {
       <div className='sixteen wide column'>
         <h1 id="solarTitle">{selectedPlanet.charAt(0).toUpperCase() + selectedPlanet.slice(1)}</h1>
       </div>
-      <div className='eight wide column'>
+      <div className='six wide column'>
         <img alt="Planet" id={selectedPlanet} style={planetStyle} src={require(`../images/${this.props.selectedPlanet}.png`)}/>
       </div>
-      <div className='eight wide column'>
+      <div className='four wide column' style={paddingFifty}>
+        <div className='infoBox' style={paddingLeft}>
+          I am the info box
+        </div>
+      </div>
+      <div className='six wide column'>
         <div style={flexBox}>
           {this.props.moons.map(moon => <Moon key={moon.id} moon={moon}/>)}
         </div>
       </div>
-      <button onClick={returnToSolarSystem}>Back to Solar System view</button>
     </div>);
   }
 }
